43b27f81c815ccdc5fd498df57a02d63fc748be2
ibrahimakdas <iaibrahimakdas@gmail.com>
2020-10-14 19:22:47 +0300

    just do it

---
+++ src/test/java/Feature1.java
@@ -0,0 +1,14 @@
+public class Feature1 {
+
+
+        public static void main(String[] args) {
+
+        }
+            //("hello everybody");
+
+            //System.out.println("............");
+
+            //System.out.println("hello cybertek");
+
+
+}
